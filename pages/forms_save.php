4606636357abcd9b152c7a9bcc8ba61a54a53abe
web20boom <vitmalina@gmail.com>
2011-10-04 12:47:45 -0600

    Android/iPad width issue fixed

--- pages/forms_save.php
+++ pages/forms_save.php
@@ -1,1 +1,1 @@
-<?print("<pre>");print_r($_POST);?>
+<body style="background-color: white">Data:<br><?print("<pre>");print_r($_POST);?></body><script>	alert(document.body.innerHTML);</script>
