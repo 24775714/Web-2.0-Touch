86435f6c2719c74046badb59cd7b3246f0e4b624
vitmalina <vitmalina@gmail.com>
2012-11-12 14:02:30 -0800

    Updated build scripts to compile cache manifest automatically

--- manifest.php
+++ manifest.php
@@ -1,5 +1,13 @@
 <?
-header("Content-Type: text/cache-manifest");
+$ignore = Array();
+$ignore[] = "./build.number";
+$ignore[] = "./build.xml";
+$ignore[] = "./cache.manifest";
+$ignore[] = "./config.xml";
+$ignore[] = "./license.txt";
+$ignore[] = "./readme.md";
+$ignore[] = "./index.appcache";
+
 $hash = "";
 $data = "# Version 1.0 Build: XXX
 
@@ -8,10 +16,15 @@ CACHE MANIFEST\n";
 // read all files and their hashes
 $dir = new RecursiveDirectoryIterator(".");
 foreach(new RecursiveIteratorIterator($dir) as $file) {
+	if (substr($file, 0, 3) == './.') continue;
+	if (substr($file, 0, 7) == './BUILD') continue;
+	if (in_array($file, $ignore)) continue;
 	if ($file->IsFile() && $file != "./manifest.php" && substr($file->getFileName(), 0, 1) != ".") $data .= $file."\n";
 	$hash .= md5_file($file);
 }
+$data .= "\n# hash: ".md5($hash);
 
 // if any file changed, then manifest should be reloaded
-print($data."# hash: ".md5($hash));
+file_put_contents("index.appcache", $data);
+//print($data."# hash: ".md5($hash));
 ?>
